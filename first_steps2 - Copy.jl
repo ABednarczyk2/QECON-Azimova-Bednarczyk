a70c319caf35b087264b64f0b6da085c97800cdb
ABednarczyk2 <a.bednarczyk2@student.uw.edu.pl>
2024-10-27 22:10:14 +0100

    almost done :3

--- first_steps2 - Copy.jl
+++ first_steps2 - Copy.jl
@@ -91,6 +91,60 @@ end
 
 my_plot = plot_powers(3) 
 
+function count_positives_broadcasting(arr)
+   
+    positives = arr .>0
+    
+   
+    count = sum(positives)
+    
+    return count
+end
+
+arr = [1, 92, -1,-1,-0.1,-3, 4, 7, -2, 0]
+count_positives2 = count_positives_broadcasting(arr)
+
+function standard_deviation(x)
+    n = length(x)                      
+    mean_x = sum(x) / n                
+    squared_diffs = (x .- mean_x) .^ 2 
+    variance = sum(squared_diffs) / n  
+    return sqrt(variance)              
+end
+
+x = [1, 2, 3, 4, 5,6,7,8,9,10]
+std_dev = standard_deviation(x)
+println(std_dev)  
+
+
+function standard_deviation(x)
+    mean_x = sum(x) / length(x)  
+    d = x .- mean_x              
+    squared_diffs = d .^ 2       
+    variance = sum(squared_diffs) / length(x)  
+    return sqrt(variance)                      
+end
+standard_deviation([1, 2, 3, 4, 5])
+
+standard_deviation([5, 10, 15])
+
 Cmatrix_data = readdlm("C:\\Users\\2022\\Desktop\\Stuff\\dataset.csv", ',')
 
+typeof(Cmatrix_data)
+size(Cmatrix_data) 
+earnings = Cmatrix_data[:, 1]      
+education = Cmatrix_data[:, 2]     
+hours_worked = Cmatrix_data[:, 3]  
 
+
+
+
+
+
+
+
+scatter(education, earnings, xlabel="Education", ylabel="Earnings",
+        title="Earnings vs Education", markercolor=:green, legend=false)
+
+scatter(hours_worked, earnings, xlabel="Hours Worked", ylabel="Earnings",
+        title="Earnings vs Hours Worked",markercolor=:red, legend=false)
